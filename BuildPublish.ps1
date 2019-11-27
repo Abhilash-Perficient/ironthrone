917c20227a223ce22a6a31884cc3fc9804a620ee
Nupur Joshi <nupur.joshi@perficient.com>
2019-11-27 19:11:03 +0530

    Changes with bifurcated folders

---
+++ BuildPublish.ps1
@@ -0,0 +1,7 @@
+$msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+$options = "/p:DeployOnBuild=true /p:PublishProfile='E:\Sitecore\ucsfPractice\Source\Project.Global\Properties\PublishProfiles\FolderProfile.pubxml' /p:VisualStudioVersion=14.0 /p:AspnetMergePath='C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools'"
+$apiproj = "E:\Sitecore\ucsfPractice\Source\Foundation.Serialization\Foundation.Serialization.csproj"
+
+# Publish the WebApi to Server...
+$build = $msbuild + " $apiproj " + $options
+Invoke-Expression $build
