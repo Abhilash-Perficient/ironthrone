954820f62b3bdd5f5efd217985651f774940267d
Nupur Joshi <nupur.joshi@perficient.com>
2019-12-23 18:17:32 +0530

    issue fixes

--- Source/Feature.VideoCarousal/Pipelines/ResolveVideoPath.cs
+++ Source/Feature.VideoCarousal/Pipelines/ResolveVideoPath.cs
@@ -14,13 +14,13 @@ namespace Feature.VideoCarousal.Pipelines
             if(args.ResultControl != null)
             {
                 LiteralControl videogallery = new LiteralControl();
-                string startstr = @"<iframe src=";
+                string startstr = @"<video class='videocomponent' controls><source src=";
                 videogallery.Text += startstr;
                 LinkField videoitm = args.ContextItem.Fields[Constants.VideoCarousel.Fields.VideoLink.id];
                 MediaItem itm = new MediaItem(videoitm.TargetItem);
                 string videolnk = Sitecore.StringUtil.EnsurePrefix('/',Sitecore.Resources.Media.MediaManager.GetMediaUrl(itm));
                 videogallery.Text += videolnk;
-                string strend = @"></iframe>";
+                string strend = @"type='video/mp4'></video>";
                 videogallery.Text += strend;
                 args.ResultControl.Controls.Add(videogallery);
             }            
